9df561bf7d2ad3d131fa18b2687c36776739e04d
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2025-07-03 09:39:36 +0530

    Time: 0 ms (100%), Space: 40.5 MB (81.35%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,10 +1,10 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-       int ans=0;
-        for(int i=1;i<=n;i++){
-            ans=(ans+k)%i;
-        }
-        return ans+1;
+      return game(n,k)+1;
+    }
+    int game(int n,int k){
+        if(n==1)return 0;
+        return (game(n-1,k)+k)%n;
     }
     
     
