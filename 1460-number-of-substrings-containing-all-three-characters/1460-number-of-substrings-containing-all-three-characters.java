5fe317548074b86abe34a04d13fc49787a61d1b6
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2025-07-15 12:53:19 +0530

    Time: 15 ms (34.69%), Space: 44.9 MB (28.72%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -3,10 +3,10 @@ class Solution {
      int[] freq = new int[s.length()];
         int left = 0;
         int count = 0;
-        for (int right = 0; right < s.length(); right++) {
-            freq[s.charAt(right) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
             while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                count += s.length() - right; 
+                count += s.length() - i; 
                 freq[s.charAt(left) - 'a']--;
                 left++;
             }
