ece2f559cd0ee850a7e4a9eeffe19e91da902c21
Rahul Pathak <156890065+RahulPathak17072005@users.noreply.github.com>
2025-09-17 11:51:45 +0530

    Time: 1 ms (100%), Space: 42.4 MB (82.19%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -5,14 +5,14 @@ class Solution {
         int[] freq=new int[26];
         char[] vowels={'a','e','i','o','u'};
         for (char ch : s.toCharArray()) {
-            if (ch >= 'a' && ch <= 'z') {
+            
                 freq[ch - 'a']++;
-            }
+            
         }
         for(char ch:vowels){
             v=Math.max(v,freq[ch-'a']);
         }
-        for (char cl = 'a'; cl <= 'z'; cl++) {
+        for (char cl : s.toCharArray()) {
             if (cl != 'a' && cl != 'e' && cl != 'i' && cl != 'o' && cl != 'u') {
                 c=Math.max(c,freq[cl-'a']);
             }
